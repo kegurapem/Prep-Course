2b96d68fa95134f2653a539384859dc48d476854
kevin gutierrez <harold.dev1993@gmail.com>
2022-08-30 01:39:49 -0500

    re-guardando

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -47,7 +47,7 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
-  return array.unshitft(elemento)
+  return array.unshift(elemento)
 }
 
 
