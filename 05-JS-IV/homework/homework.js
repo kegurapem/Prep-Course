79444ab21c2ebdf2835cb76903e2beeeaf2e2e6a
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-09-05 18:05:25 -0500

    avanzando homework 05-JS-IV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -10,9 +10,10 @@ function crearGato (nombre, edad) {
     nombre: nombre,
     edad: edad,
     meow: function(){
-      return 'Meow!'
+      return 'Meow!';
     }
   }
+  return newObjeto
 }
 
 function agregarPropiedad (objeto, property) {
@@ -20,6 +21,9 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto[property] = null;
+  // objeto.property = null;
+  return objeto;
 }
 
 function invocarMetodo (objeto, metodo) {
