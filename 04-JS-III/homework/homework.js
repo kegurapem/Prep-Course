73a9d320057f9298f47e6642e0b0947db3a52eea
kevin gutierrez <harold.dev1993@gmail.com>
2022-09-05 00:56:35 -0500

    iniciando  homework 05-JS-IV

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -119,7 +119,7 @@ function numeroMasGrande(numeros) {
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
-   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
+  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
   producto = 1
   if (arguments.length === 0) {
